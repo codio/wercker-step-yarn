0561ea80dee08743ee852c93ca232ddc685ef25f
Maksim Kraev <maxim.kraev@gmail.com>
2016-12-14 10:42:36 +0000

    try direct command

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-YARN=yarn
+YARN="$HOME/.yarn/bin/yarn"
 #if ! hash yarn 2>/dev/null; then
 echo "Installing yarn..."
 curl -o- -L https://yarnpkg.com/install.sh | bash
 #fi
 
-PATH="$HOME/.yarn/bin:$PATH"
 YARN="HOME=$WERCKER_CACHE_DIR $YARN"
 
 YARN="$YARN $WERCKER_YARN_OPTIONS"
