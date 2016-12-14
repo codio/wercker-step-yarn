c1f6b88002d6d9f1aa71d412b2e5abec2ebb2082
Maksim Kraev <maxim.kraev@gmail.com>
2016-12-14 10:39:56 +0000

    add path for yarn

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 YARN=yarn
+#if ! hash yarn 2>/dev/null; then
+echo "Installing yarn..."
+curl -o- -L https://yarnpkg.com/install.sh | bash
+#fi
 
-if ! hash yarn 2>/dev/null; then
-  echo "Installing yarn..."
-  curl -o- -L https://yarnpkg.com/install.sh | bash
-fi
-
+PATH="$HOME/.yarn/bin:$PATH"
 YARN="HOME=$WERCKER_CACHE_DIR $YARN"
 
 YARN="$YARN $WERCKER_YARN_OPTIONS"
